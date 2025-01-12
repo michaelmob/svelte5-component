23dd542d0d391d0266bad87ceaa99a1f86d8c92e
Michael <4530010+michaelmob@users.noreply.github.com>
2025-01-12 17:45:47 +0000

    Add example of a persisted state hit counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+  import { PersistedState } from "runed";
+  const hitCounter = new PersistedState("hit-counter", 0);
+  hitCounter.current++;
+
   let value = $state(0);
 </script>
 
 <div>
-  <input type="number" bind:value />
-  <button onclick={() => value++}>+</button>
-  <button onclick={() => value--}>-</button>
+  <div>{hitCounter.current} hits</div>
+  <div>
+    <input type="number" bind:value />
+    <button onclick={() => value++}>+</button>
+    <button onclick={() => value--}>-</button>
+  </div>
 </div>
