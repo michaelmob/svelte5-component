18f577634640508637b2f15b07bcab290a416d86
michaelmob <4530010+michaelmob@users.noreply.github.com>
2025-03-12 20:32:24 -0400

    update dependencies

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,8 @@
 <script>
-  import { PersistedState } from "runed";
-  const hitCounter = new PersistedState("hit-counter", 0);
-  hitCounter.current++;
-
   let value = $state(0);
 </script>
 
 <div>
-  <div>{hitCounter.current} hits</div>
   <div>
     <input type="number" bind:value />
     <button onclick={() => value++}>+</button>
